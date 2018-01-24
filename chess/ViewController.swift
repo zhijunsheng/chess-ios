f3f1dc61313e4d8e4c607f56887a5a2e32ce1ff0
Zhijun Sheng <zhijunsheng@gmail.com>
2018-01-23 20:10:29 -0500

    created xcode project

---
+++ chess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chess
+//
+//  Created by Donald Sheng on 2018-01-23.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
