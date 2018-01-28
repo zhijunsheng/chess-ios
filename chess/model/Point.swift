525061dbe2f7f574a6c862b0b03048bcef804e4e
Bowen Lin <bowenblinblue@gmail.com>
2018-01-27 21:47:56 -0500

    Created test cases.

---
+++ chess/model/Point.swift
@@ -0,0 +1,18 @@
+//
+//  Point.swift
+//  chess
+//
+//  Created by Bowen Lin on 2018-01-27.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Point {
+    
+    var row: Int
+    var col: Int
+
+
+
+}
---
+++ chessTests/PointTests.swift
@@ -0,0 +1,36 @@
+//
+//  PointTests.swift
+//  chessTests
+//
+//  Created by Bowen Lin on 2018-01-27.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+@testable import chess
+
+class PointTests: XCTestCase {
+    
+    
+    func testExample() {
+//        print("//////////")
+        let point = Point(row: 3, col: 5)
+        
+//        print(point.row)
+//        print(point.col)
+        
+        XCTAssertEqual(3, point.row)
+        XCTAssertEqual(5, point.col)
+        
+        
+    }
+    
+    func testAdd() {
+        let sum = add(a: 6, b: 7)
+        XCTAssertEqual(13, sum)
+    }
+    
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
