b776e8d9fd33cee25f57faf0ae8232cdeb79f7cf
daobao <dabao@daobaodeMacBook-Air.local>
2018-12-09 08:59:06 +0800

    copied some files from old project

---
+++ chess/BoardView.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+class BoardView: UIView {
+    
+    let boardOriginX: CGFloat = 33
+    let boardOriginY: CGFloat = 38
+    let cellSide: CGFloat = 28
+    
+    override func draw(_ rect: CGRect) {
+
+        drawChessBoard()
+        
+
+    }
+    
+    func drawChessBoard() {
+        
+        
+        drawColumn(idx: 0)
+        drawColumn(idx: 1)
+        drawColumn(idx: 2)
+        drawColumn(idx: 3)
+        drawColumn(idx: 4)
+        drawColumn(idx: 5)
+        drawColumn(idx: 6)
+        drawColumn(idx: 7)
+        
+    }
+    
+    func drawColumn(idx:Int) {
+        for i in 0..<8 {
+            let color : UIColor
+            if (i + idx) % 2 == 0 {
+                color = .red
+            } else {
+                color = .green
+            }
+            
+            drawSquare(x: boardOriginX + CGFloat(idx) * cellSide, y: boardOriginY + CGFloat(i) * cellSide, color: color)
+        }
+    }
+    
+    func drawSquare(x: CGFloat, y: CGFloat, color: UIColor) {
+        let j10 = UIBezierPath()
+        j10.move(to: CGPoint(x: x, y: y))
+        j10.addLine(to: CGPoint(x: x + cellSide, y: y))
+        j10.addLine(to: CGPoint(x: x + cellSide, y: y + cellSide))
+        j10.addLine(to: CGPoint(x: x, y: y + cellSide))
+        j10.close()
+        
+        color.setFill()
+        j10.fill()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
