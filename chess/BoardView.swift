0b9741bc91371ffaf25d152c2e5ee45ff5ce23b9
Elaine <elaine@Elaines-MacBook.local>
2019-03-18 09:01:34 +0800

    Made a function to draw square do not need to draw square over and over and over again.

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -9,25 +9,35 @@
 import UIKit
 
 class BoardView: UIView {
-    
+    let originXForBoard: CGFloat = 110.0000000000
+    let originYForFirstSquare: CGFloat = 150.0000000000000
     let cellSide: CGFloat = 30
     
-
     override func draw(_ rect: CGRect) {
         
+        drawSquare(locationX: originXForBoard, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        drawSquare(locationX: 0, locationY: 0, colourLiteral: #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1))
+        
+        drawSquare(locationX: 30, locationY: 30, colourLiteral: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+    
+    
+    
+    
+    }
+    
+    func drawSquare(locationX: CGFloat, locationY : CGFloat, colourLiteral: UIColor) {
         let pencil = UIBezierPath()
         
-        pencil.move(to: CGPoint(x: 110, y: 150))
-        pencil.addLine(to: CGPoint(x: 110 + cellSide, y: 150))
-        pencil.addLine(to: CGPoint(x: 110 + cellSide, y: 150 + cellSide))
-        pencil.addLine(to: CGPoint(x: 110, y: 150 + cellSide))
+        pencil.move(to: CGPoint(x: locationX, y: locationY))
+        pencil.addLine(to: CGPoint(x: locationX + cellSide, y: locationY))
+        pencil.addLine(to: CGPoint(x: locationX + cellSide , y: locationY + cellSide))
+        pencil.addLine(to: CGPoint(x: locationX , y: locationY + cellSide))
         pencil.close()
         
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        colourLiteral.setFill()
         pencil.fill()
-        
-       
+        pencil.stroke()
     }
-  
-
 }
