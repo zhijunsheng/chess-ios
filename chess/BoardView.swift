e986dac7645c18a0d7b30f3db36c0c47be45a554
Elaine <elaine@Elaines-MacBook.local>
2019-03-31 19:13:20 -0400

    Made easier with drawsquare function.

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -1,11 +1,3 @@
-//
-//  BoardView.swift
-//  chess
-//
-//  Created by Elaine on 2019/3/13.
-//  Copyright © 2019 GoldThumb Inc. All rights reserved.
-//
-
 import UIKit
 
 class BoardView: UIView {
@@ -16,25 +8,25 @@ class BoardView: UIView {
     let cellSide: CGFloat = 36
     override func draw(_ rect: CGRect) {
         
+        for i in 0...3 {
+             drawSquare(locationX: originXForBoard + cellSide * CGFloat(i) * 2 , locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
+            
+             drawSquare(locationX: originXForBoard + cellSide * (1 + CGFloat(i) * 2), locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
+            
+             drawSquare(locationX: originXForBoard + cellSide * ( 1 + CGFloat(i) * 2) , locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
+            
+            drawSquare(locationX: originXForBoard + cellSide * CGFloat(i) * 2, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
+            
+        }
+        // i -> 1 + i * 2
+        // 0 -> 1
+        // 1 -> 3
+        // 2 -> 5
+        // 3 -> 7
+        for i in 0...3 {
+           
+        }
         
-        drawSquare(locationX: originXForBoard, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 2, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 3, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 4, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 5, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 6, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide * 7, locationY: originYForFirstSquare, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-//
-        
-        drawSquare(locationX: originXForBoard , locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + 2 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + 3 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + 4 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + 5 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
-        drawSquare(locationX: originXForBoard + 6 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
-        drawSquare(locationX: originXForBoard + 7 * cellSide, locationY: originYForFirstSquare + cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
 //
         drawSquare(locationX: originXForBoard , locationY: originYForFirstSquare + 2 * cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
         drawSquare(locationX: originXForBoard + cellSide, locationY: originYForFirstSquare + 2 * cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
@@ -66,7 +58,7 @@ class BoardView: UIView {
         drawSquare(locationX: originXForBoard + 6 * cellSide, locationY: originYForFirstSquare + 4 * cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
         drawSquare(locationX: originXForBoard + 7 * cellSide, locationY: originYForFirstSquare + 4 * cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
 //
-    
+
         drawSquare(locationX: originXForBoard , locationY: originYForFirstSquare + 5 * cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
         drawSquare(locationX: originXForBoard + cellSide, locationY: originYForFirstSquare + 5 * cellSide, colourLiteral: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
         drawSquare(locationX: originXForBoard + 2 * cellSide, locationY: originYForFirstSquare + 5 * cellSide, colourLiteral: #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1))
