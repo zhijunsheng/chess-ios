35f6a602e480a7f83cff85b806c2a4053d99085e
Elaine <elainexu392@gmail.com>
2019-04-07 19:03:00 -0400

    MAde real pieces for chess board.

--- chess/GameViewController.swift
+++ chess/GameViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var boardView: BoardView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // ringook_chess_b
+        // queen_chess_b
         
+        let wreck = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 3, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage = UIImage(named: "queen_chess_b")
+        let pieceImageView = UIImageView(frame: wreck)
+        pieceImageView.image = fightingImage
+        boardView.addSubview(pieceImageView)
+        
+        let wreck1 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 4, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage1 = UIImage(named: "king_chess_b")
+        let pieceImageView1 = UIImageView(frame: wreck1)
+        pieceImageView1.image = fightingImage1
+        boardView.addSubview(pieceImageView1)
+        
+        let wreck2 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 4, y: boardView.originYForFirstSquare + boardView.cellSide * 7, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage2 = UIImage(named: "king_chess_w")
+        let pieceImageView2 = UIImageView(frame: wreck2)
+        pieceImageView2.image = fightingImage2
+        boardView.addSubview(pieceImageView2)
+        
+        let wreck3 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 6, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage3 = UIImage(named: "knight_chess_b")
+        let pieceImageView3 = UIImageView(frame: wreck3)
+        pieceImageView3.image = fightingImage3
+        boardView.addSubview(pieceImageView3)
     }
 
 }
