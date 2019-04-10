9577031241931425b25609e5117679044ded3797
Elaine <elainexu392@gmail.com>
2019-04-09 20:56:38 -0400

    MAde 2 rooks on opposite ends using i * 7 and for loop.

--- chess/GameViewController.swift
+++ chess/GameViewController.swift
@@ -43,6 +43,52 @@ class GameViewController: UIViewController {
         let pieceImageView3 = UIImageView(frame: wreck3)
         pieceImageView3.image = fightingImage3
         boardView.addSubview(pieceImageView3)
+        
+        // i -> i * 7
+        // 0 -> 0
+        // 1 -> 7
+        for i in 0 ..< 2 {
+            
+            let wreck7 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * CGFloat(i * 7), y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+            let fightingImage7 = UIImage(named: "rook_chess_b")
+            let pieceImageView7 = UIImageView(frame: wreck7)
+            pieceImageView7.image = fightingImage7
+            boardView.addSubview(pieceImageView7)
+        }
+        
+
+        
+        
+        
+        
+        
+        let wreck5 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 2, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage5 = UIImage(named: "bishop_chess_b")
+        let pieceImageView5 = UIImageView(frame: wreck5)
+        pieceImageView5.image = fightingImage5
+        boardView.addSubview(pieceImageView5)
+        
+        let wreck6 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 1, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage6 = UIImage(named: "knight_chess_b")
+        let pieceImageView6 = UIImageView(frame: wreck6)
+        pieceImageView6.image = fightingImage6
+        boardView.addSubview(pieceImageView6)
+
+        
+        let wreck8 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * 5, y: boardView.originYForFirstSquare, width: boardView.cellSide, height: boardView.cellSide)
+        let fightingImage8 = UIImage(named: "bishop_chess_b")
+        let pieceImageView8 = UIImageView(frame: wreck8)
+        pieceImageView8.image = fightingImage8
+        boardView.addSubview(pieceImageView8)
+        
+        for i in 0 ..< 8 {
+            let wreck9 = CGRect(x:  boardView.originXForBoard + boardView.cellSide * CGFloat(i), y: boardView.originYForFirstSquare + boardView.cellSide, width: boardView.cellSide, height: boardView.cellSide)
+            let fightingImage9 = UIImage(named: "pawn_chess_b")
+            let pieceImageView9 = UIImageView(frame: wreck9)
+            pieceImageView9.image = fightingImage9
+            boardView.addSubview(pieceImageView9)
+            
+        }
     }
 
 }
