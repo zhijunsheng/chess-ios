a1f8eb0a9da18d5d9837ee87ebd28eadd69cf0ad
Hal Wang <halhaoranwang@gmail.com>
2019-09-15 11:27:35 +0800

    we added ChessPiece and ChessPieceRank

---
+++ chess/ChessPiece.swift
@@ -0,0 +1,21 @@
+//
+//  ChessPiece.swift
+//  chess
+//
+//  Created by Halwong on 2019/9/15.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct ChessPiece {
+    let rank: ChessPieceRank
+    let col: Int
+    let row: Int
+    let isWhite: Bool
+    let imgName: String
+}
+
+    
+
+
---
+++ chess/ChessPieceRank.swift
@@ -0,0 +1,19 @@
+//
+//  ChessPieceRank.swift
+//  chess
+//
+//  Created by Halwong on 2019/9/15.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+enum ChessPieceRank {
+    case rook
+    case bishop
+    case king
+    case knight
+    case pawn
+    case queen
+    
+}
