de2c41bdd4c658ed09a7f0e9cadb4481b0aef4ce
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-09-26 15:42:53 -0500

    added a square

---
+++ chess/BoardView.swift
@@ -0,0 +1,19 @@
+//
+//  BoardView.swift
+//  chess
+//
+//  Created by Grace Huang on 9/26/19.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+   
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
