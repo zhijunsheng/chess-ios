1774368fd6e4eb56be02523fa20689f086a0f1d0
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-09-26 16:17:32 -0500

    started making the board

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class BoardView: UIView {
 
-   
+    let originX: CGFloat = 15
+    let originY: CGFloat = 15
+    let cellSide: CGFloat = 20
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+
+        
+        let penS1 = UIBezierPath(rect: CGRect(x: originX, y: originY, width: 160, height: 160))
+        penS1.stroke()
+        
+        drawSquareAt(col: 1, row: 0)
+        drawSquareAt(col: 3, row: 0)
+        
     }
     
 
+    func drawSquareAt(col:Int, row:Int) {
+        let penS3 = UIBezierPath(rect: CGRect(x: originX + cellSide * CGFloat(col), y: originY, width: cellSide, height: cellSide))
+        penS3.fill()
+    }
 }
