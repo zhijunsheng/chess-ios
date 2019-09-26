9dd8c1da9cb51c61f1dde175ee766068d0ae22ca
ethan <ethanyiyaozhang@gmail.com>
2019-09-26 20:36:02 +0800

    We removed pieces.

---
+++ chessTests/CollatzTests.swift
@@ -0,0 +1,39 @@
+//
+//  CollatzTests.swift
+//  chessTests
+//
+//  Created by ethan on 2019/4/20.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+
+class CollatzTests: XCTestCase {
+
+
+    /*
+ 
+     3
+     5
+     7
+     9
+     11
+ 
+    */
+    func testCollatz() {
+//        for i in 1..<6 {
+//            print("\(i)")
+//        }
+        
+        // while
+//        var j = 1
+//        while j < 6 {
+//            print("\(j)")
+//            j = j + 1
+//        }
+        
+        
+    }
+
+
+}
