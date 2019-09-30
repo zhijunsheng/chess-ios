13060a2930fd5510baf81c79b72a2e26f1912c2f
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-09-29 19:31:23 -0500

    made the squares

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -12,22 +12,54 @@ class BoardView: UIView {
 
     let originX: CGFloat = 15
     let originY: CGFloat = 15
-    let cellSide: CGFloat = 20
+    let cellSide: CGFloat = 33
     
     override func draw(_ rect: CGRect) {
 
         
-        let penS1 = UIBezierPath(rect: CGRect(x: originX, y: originY, width: 160, height: 160))
+        let penS1 = UIBezierPath(rect: CGRect(x: originX, y: originY, width: 264, height: 264))
         penS1.stroke()
         
         drawSquareAt(col: 1, row: 0)
         drawSquareAt(col: 3, row: 0)
+        drawSquareAt(col: 5, row: 0)
+        drawSquareAt(col: 7, row: 0)
+        drawSquareAt(col: 0, row: 1)
+        drawSquareAt(col: 2, row: 1)
+        drawSquareAt(col: 4, row: 1)
+        drawSquareAt(col: 6, row: 1)
+        drawSquareAt(col: 1, row: 2)
+        drawSquareAt(col: 3, row: 2)
+        drawSquareAt(col: 5, row: 2)
+        drawSquareAt(col: 7, row: 2)
+        drawSquareAt(col: 0, row: 3)
+        drawSquareAt(col: 2, row: 3)
+        drawSquareAt(col: 4, row: 3)
+        drawSquareAt(col: 6, row: 3)
+        drawSquareAt(col: 1, row: 4)
+        drawSquareAt(col: 3, row: 4)
+        drawSquareAt(col: 5, row: 4)
+        drawSquareAt(col: 7, row: 4)
+        drawSquareAt(col: 0, row: 5)
+        drawSquareAt(col: 2, row: 5)
+        drawSquareAt(col: 4, row: 5)
+        drawSquareAt(col: 6, row: 5)
+        drawSquareAt(col: 1, row: 6)
+        drawSquareAt(col: 3, row: 6)
+        drawSquareAt(col: 5, row: 6)
+        drawSquareAt(col: 7, row: 6)
+        drawSquareAt(col: 0, row: 7)
+        drawSquareAt(col: 2, row: 7)
+        drawSquareAt(col: 4, row: 7)
+        drawSquareAt(col: 6, row: 7)
+       
         
-    }
+        }
     
 
     func drawSquareAt(col:Int, row:Int) {
-        let penS3 = UIBezierPath(rect: CGRect(x: originX + cellSide * CGFloat(col), y: originY, width: cellSide, height: cellSide))
+        let penS3 = UIBezierPath(rect: CGRect(x:(originX + cellSide * CGFloat(col)), y: (originY + cellSide * CGFloat(row)), width: cellSide, height: cellSide))
+            
         penS3.fill()
     }
 }
