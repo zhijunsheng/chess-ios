d5236ee4432fe5e4454055c65291483f40535839
ethan <ethanyiyaozhang@gmail.com>
2019-10-03 20:32:16 +0800

    We improved pieces.

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -3,11 +3,14 @@ import UIKit
 
 class BoardView: UIView {
 
-    let originX: CGFloat = 30.0
-    let originY: CGFloat = 150.0
-    let cellSide: CGFloat = 30.0
+    var originX: CGFloat = 123_456_789_123_456_789.0
+    var originY: CGFloat = 173_738_383_838_839_498.0
+    let cellSide: CGFloat = 80.0
     
     override func draw(_ rect: CGRect) {
+        
+        originX = (bounds.width - cellSide * 8) / 2
+        originY = (bounds.height - cellSide * 8) / 2
         
         let  feather = UIBezierPath()
         
@@ -322,5 +325,38 @@ class BoardView: UIView {
 
         #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1).setFill()
         pen.fill()
+        
+        let queen_chess_bImg = UIImage(named: "queen_chess_b")
+        let king_chess_bImg = UIImage(named: "king_chess_b")
+        let king_chess_wImg = UIImage(named: "king_chess_w")
+        let queen_chess_wImg = UIImage(named: "queen_chess_w")
+        let rook_chess_wImg = UIImage(named: "rook_chess_w")
+        let knight_chess_wImg = UIImage(named: "knight_chess_w")
+        let bishop_chess_wImg = UIImage(named: "bishop_chess_w")
+        let rook_chess_bImg = UIImage(named: "rook_chess_b")
+        let knight_chess_bImg = UIImage(named: "knight_chess_b")
+        let bishop_chess_bImg = UIImage(named: "bishop_chess_b")
+        let pawn_chess_wImg = UIImage(named: "pawn_chess_w")
+        let pawn_chess_bImg = UIImage(named: "pawn_chess_b")
+        
+        king_chess_wImg?.draw(in: CGRect(x: originX + 3 * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+        queen_chess_wImg?.draw(in: CGRect(x: originX + 4 * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+        king_chess_bImg?.draw(in: CGRect(x: originX + 3 * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+        queen_chess_bImg?.draw(in: CGRect(x: originX + 4 * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+        
+        for i in 0..<2 {
+            rook_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(7 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+            knight_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(1 + 5 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+            bishop_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(2 + 3 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+            
+            rook_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(7 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+            knight_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(1 + 5 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+            bishop_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(2 + 3 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+        }
+        
+        for i in 0..<8 {
+            pawn_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(i) * cellSide, y: originY + 6 * cellSide, width: cellSide, height: cellSide))
+            pawn_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(i) * cellSide, y: originY + 1 * cellSide, width: cellSide, height: cellSide))
+        }
     }
 }
