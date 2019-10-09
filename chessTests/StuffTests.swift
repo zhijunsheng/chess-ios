aaf1150699fe0fb8c8e70de235ef110c1c308ab5
Peter Shi <petershi@Peters-MacBook-Pro-624.local>
2019-10-08 22:25:57 -0400

    made new way to do wierd question

--- chessTests/StuffTests.swift
+++ chessTests/StuffTests.swift
@@ -10,6 +10,19 @@ import XCTest
 
 class StuffTests: XCTestCase {
     
+    /*
+ 
+     ABCDE * 4 = EDCBA
+     
+     ABCDE
+     *   4
+    --------
+     EDCBA
+ 
+    */
+    
+    
+    
     func testThing() {
         var count = 0
         for a in 1...9 {
@@ -29,4 +42,26 @@ class StuffTests: XCTestCase {
         print(count)
     }
     
+    func testThingy() {
+        
+        for i in 10000...24999 {
+            let onesDigit = i - i / 10 * 10
+            
+            let tensDigit = (i - i / 100 * 100) / 10
+            
+            let hundredsDigit = (i - i / 1000 * 1000) / 100
+            
+            let thousandsDigit = (i - i / 10000 * 10000) / 1000
+            
+            let tenThousandsDigit = (i - i / 100000 * 100000) / 10000
+            
+            let thing = onesDigit + tensDigit * 10 + hundredsDigit * 100 + thousandsDigit * 1000 + tenThousandsDigit * 10000
+            
+            let thingy = tenThousandsDigit + thousandsDigit * 10 + hundredsDigit * 100 + tensDigit * 1000 + onesDigit * 10000
+            
+            if thing * 4 == thingy {
+                print(thing)
+            }
+        }
+    }
 }
