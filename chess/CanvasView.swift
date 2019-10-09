28f646ff62d6e224d2916038d24c99f482db0cda
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-10-09 17:14:11 -0400

    I made a 400x400 square for the chess game.

---
+++ chess/CanvasView.swift
@@ -0,0 +1,21 @@
+//
+//  CanvasView.swift
+//  chess
+//
+//  Created by Angie Yan on 2019-10-09.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+ 
+
+}
