e17bb717250e7f8527eca7570d89a5efd447dd2f
Hal Wang <halhaoranwang@gmail.com>
2019-10-13 11:08:03 +0800

    we improved the for loop

--- chessTests/MathTests.swift
+++ chessTests/MathTests.swift
@@ -11,12 +11,26 @@ import XCTest
 class MathTests: XCTestCase {
     func testMath() {
         /*
+         So, manually we cannot try all the numbers because there
+         are too many of them.
  
-ABCDE * 4 = EDCBA
+         ABCDE * 4 = EDCBA
+         
+         A = 2
+         E = 8
+         B = 1
+         D = 7
+         c = 9
+         
+          333
+         21978
+         *   4
+         -------
+         87912
          
         */
-        for i in 0 ..< 100000 {
-            let x = 10000 + i
+        
+        for x in 10000 ..< 25000 {
             let onesPlace = x % 10
             let tensPlace = (x % 100) / 10
             let hundredsPlace = (x % 1000) / 100
@@ -28,22 +42,8 @@ ABCDE * 4 = EDCBA
             let z = onesPlace * 10000 + tensPlace * 1000 + hundredsPlace * 100 + thousandsPlace * 10 + tenThousandsPlace * 1
             
             if y * 4 == z {
-                print("\(y), \(z)")
+                print("\(y) * 4 = \(z)")
             }
         }
-//        let x = 10000
-//        let onesPlace = x % 10
-//        let tensPlace = (x % 100) / 10
-//        let hundredsPlace = (x % 1000) / 100
-//        let thousandsPlace = (x % 10000) / 1000
-//        let tenThousandsPlace = (x % 100000) / 10000
-//
-//        print("\(onesPlace), \(tensPlace), \(hundredsPlace), \(thousandsPlace), \(tenThousandsPlace)")
-//
-//        let y = tenThousandsPlace * 10000 + thousandsPlace * 1000 + hundredsPlace * 100 + tensPlace * 10 + onesPlace * 1
-//        print(y)
-//
-//        let z = onesPlace * 10000 + tensPlace * 1000 + hundredsPlace * 100 + thousandsPlace * 10 + tenThousandsPlace * 1
-//        print(z)
     }
 }
