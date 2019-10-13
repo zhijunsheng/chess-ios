47e46e41977dc36006d4711f60684b0091551945
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-10-13 18:09:37 -0400

    I changed the height and width of the turquoise canvas

--- chess/CanvasView.swift
+++ chess/CanvasView.swift
@@ -14,7 +14,14 @@ class CanvasView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
+        let bPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 100, height: 120))
+        bPath.fill()
+        
+        let bPath2 = UIBezierPath(rect: CGRect(x: 90 + 200, y: 100 + 50, width: 100, height: 100))
+        #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setFill()
+        bPath2.fill()
+   
     }
  
 
