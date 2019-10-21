0545220c873f86d3f512fde0dd55edd7f826d126
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-10-20 20:35:58 -0500

    made ChessBoardTests and printed empty board

---
+++ chess/ChessBoard.swift
@@ -0,0 +1,44 @@
+//
+//  ChessBoard.swift
+//  chess
+//
+//  Created by Grace Huang on 10/20/19.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct ChessBoard: CustomStringConvertible{
+    
+    /*
+     
+
+     0 1 2 3 4 5 6 7
+   0 . . . . . . . .
+   1 . . . . . . . .
+   2 . . . . . . . .
+   3 . . . . . . . .
+   4 . . . . . . . .
+   5 . . . . . . . .
+   6 . . . . . . . .
+   7 . . . . . . . .
+     
+    */
+    var description: String {
+        var desc: String = ""
+        
+        for j in 0..<8 {
+            for i in 0..<8 {
+                desc += ". "
+            }
+            desc += "\n"
+        }
+        
+        
+        
+        return desc
+    }
+    
+    
+}
+
---
+++ chessTests/ChessBoardTests.swift
@@ -0,0 +1,20 @@
+//
+//  ChessBoardTests.swift
+//  chessTests
+//
+//  Created by Grace Huang on 10/20/19.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+@testable import chess
+
+class ChessBoardTests: XCTestCase {
+
+    func testPrintingChessBoard() {
+        let chessBoard = ChessBoard()
+        print(chessBoard)
+    }
+    
+
+}
