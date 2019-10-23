4c040c1d125f1e0afe252cc033b2b46bfb105167
daobao <lucashongyuanzhang@gmail.com>
2019-10-23 20:28:30 +0800

    we can move the piecen

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -10,12 +10,16 @@ class ViewController: UIViewController, ChessDelegate {
     func movePiece(frX: Int, frY: Int, toX: Int, toY: Int) {
         print("🌩chess🌩")
         chessBrain.movePiece(frX: frX, frY: frY, toX: toX, toY: toY)
+        boardView.piecesBoxShadow = chessBrain.piecesBox
+        boardView.setNeedsDisplay()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         chessBrain.reset()
         boardView.piecesBoxShadow = chessBrain.piecesBox
+        boardView.setNeedsDisplay()
+        
         boardView.chessDelegate = self
         
     }
