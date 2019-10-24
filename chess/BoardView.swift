1614f5ac327c1c190e1bd88e472b35d7acb80520
Andy Yan <andy@Andys-MacBook-Air.local>
2019-10-23 21:31:01 -0400

    we used a for loop for our chess board

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class BoardView: UIView {
+    func drawPiece(col:Int, row:Int, imageName:String)  {
+       let pieceImage = UIImage(named: imageName)
+        pieceImage?.draw(in: CGRect(x: col * 80, y: row * 80, width: 80, height: 80))
+    }
 
-    
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
+        
+        
+        
         let pencil = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil.fill()
         
         let pencil01 = UIBezierPath(rect: CGRect(x: 80, y: 0, width: 80, height: 80))
@@ -24,7 +28,7 @@ class BoardView: UIView {
         pencil01.fill()
         
         let pencil02 = UIBezierPath(rect: CGRect(x: 160, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).setFill()
         pencil02.fill()
         
         let pencil03 = UIBezierPath(rect: CGRect(x: 240, y: 0, width: 80, height: 80))
@@ -32,7 +36,7 @@ class BoardView: UIView {
         pencil03.fill()
         
         let pencil04 = UIBezierPath(rect: CGRect(x: 320, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).setFill()
         pencil04.fill()
         
         let pencil05 = UIBezierPath(rect: CGRect(x: 400, y: 0, width: 80, height: 80))
@@ -40,7 +44,7 @@ class BoardView: UIView {
         pencil05.fill()
         
         let pencil06 = UIBezierPath(rect: CGRect(x: 480, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).setFill()
         pencil06.fill()
         
         let pencil07 = UIBezierPath(rect: CGRect(x: 560, y: 0, width: 80, height: 80))
@@ -48,7 +52,7 @@ class BoardView: UIView {
         pencil07.fill()
         
         let pencil08 = UIBezierPath(rect: CGRect(x: 640, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil08.fill()
         
         let pencil09 = UIBezierPath(rect: CGRect(x: 0, y: 80, width: 80, height: 80))
@@ -56,7 +60,7 @@ class BoardView: UIView {
         pencil09.fill()
         
         let pencil10 = UIBezierPath(rect: CGRect(x: 80, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil10.fill()
         
         let pencil11 = UIBezierPath(rect: CGRect(x: 160, y: 80, width: 80, height: 80))
@@ -64,7 +68,7 @@ class BoardView: UIView {
         pencil11.fill()
         
         let pencil12 = UIBezierPath(rect: CGRect(x: 240, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil12.fill()
         
         let pencil13 = UIBezierPath(rect: CGRect(x: 320, y: 80, width: 80, height: 80))
@@ -72,7 +76,7 @@ class BoardView: UIView {
         pencil13.fill()
         
         let pencil14 = UIBezierPath(rect: CGRect(x: 400, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil14.fill()
         
         let pencil15 = UIBezierPath(rect: CGRect(x: 480, y: 80, width: 80, height: 80))
@@ -80,11 +84,11 @@ class BoardView: UIView {
         pencil15.fill()
         
         let pencil16 = UIBezierPath(rect: CGRect(x: 560, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil16.fill()
         
         let pencil17 = UIBezierPath(rect: CGRect(x: 0, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil17.fill()
         
         let pencil18 = UIBezierPath(rect: CGRect(x: 80, y: 160, width: 80, height: 80))
@@ -92,7 +96,7 @@ class BoardView: UIView {
         pencil18.fill()
         
         let pencil19 = UIBezierPath(rect: CGRect(x: 160, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil19.fill()
         
         let pencil20 = UIBezierPath(rect: CGRect(x: 240, y: 160, width: 80, height: 80))
@@ -100,7 +104,7 @@ class BoardView: UIView {
         pencil20.fill()
         
         let pencil21 = UIBezierPath(rect: CGRect(x: 320, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil21.fill()
         
         let pencil22 = UIBezierPath(rect: CGRect(x: 400, y: 160, width: 80, height: 80))
@@ -108,7 +112,7 @@ class BoardView: UIView {
         pencil22.fill()
         
         let pencil23 = UIBezierPath(rect: CGRect(x: 480, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil23.fill()
         
         let pencil24 = UIBezierPath(rect: CGRect(x: 560, y: 160, width: 80, height: 80))
@@ -120,7 +124,7 @@ class BoardView: UIView {
         pencil25.fill()
         
         let pencil26 = UIBezierPath(rect: CGRect(x: 80, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil26.fill()
         
         let pencil27 = UIBezierPath(rect: CGRect(x: 160, y: 240, width: 80, height: 80))
@@ -128,7 +132,7 @@ class BoardView: UIView {
         pencil27.fill()
         
         let pencil28 = UIBezierPath(rect: CGRect(x: 240, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil28.fill()
         
         let pencil29 = UIBezierPath(rect: CGRect(x: 320, y: 240, width: 80, height: 80))
@@ -136,7 +140,7 @@ class BoardView: UIView {
         pencil29.fill()
         
         let pencil30 = UIBezierPath(rect: CGRect(x: 400, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil30.fill()
         
         let pencil31 = UIBezierPath(rect: CGRect(x: 480, y: 240, width: 80, height: 80))
@@ -144,11 +148,11 @@ class BoardView: UIView {
         pencil31.fill()
         
         let pencil32 = UIBezierPath(rect: CGRect(x: 560, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil32.fill()
         
         let pencil33 = UIBezierPath(rect: CGRect(x: 0, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil33.fill()
         
         let pencil34 = UIBezierPath(rect: CGRect(x: 80, y: 320, width: 80, height: 80))
@@ -156,7 +160,7 @@ class BoardView: UIView {
         pencil34.fill()
         
         let pencil35 = UIBezierPath(rect: CGRect(x: 160, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil35.fill()
         
         let pencil36 = UIBezierPath(rect: CGRect(x: 240, y: 320, width: 80, height: 80))
@@ -164,7 +168,7 @@ class BoardView: UIView {
         pencil36.fill()
         
         let pencil37 = UIBezierPath(rect: CGRect(x: 320, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil37.fill()
         
         let pencil38 = UIBezierPath(rect: CGRect(x: 400, y: 320, width: 80, height: 80))
@@ -172,7 +176,7 @@ class BoardView: UIView {
         pencil38.fill()
         
         let pencil39 = UIBezierPath(rect: CGRect(x: 480, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil39.fill()
         
         let pencil40 = UIBezierPath(rect: CGRect(x: 560, y: 320, width: 80, height: 80))
@@ -184,7 +188,7 @@ class BoardView: UIView {
         pencil41.fill()
         
         let pencil42 = UIBezierPath(rect: CGRect(x: 80, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil42.fill()
         
         let pencil43 = UIBezierPath(rect: CGRect(x: 160, y: 400, width: 80, height: 80))
@@ -192,7 +196,7 @@ class BoardView: UIView {
         pencil43.fill()
         
         let pencil44 = UIBezierPath(rect: CGRect(x: 240, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil44.fill()
         
         let pencil45 = UIBezierPath(rect: CGRect(x: 320, y: 400, width: 80, height: 80))
@@ -200,7 +204,7 @@ class BoardView: UIView {
         pencil45.fill()
         
         let pencil46 = UIBezierPath(rect: CGRect(x: 400, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil46.fill()
         
         let pencil47 = UIBezierPath(rect: CGRect(x: 480, y: 400, width: 80, height: 80))
@@ -208,11 +212,11 @@ class BoardView: UIView {
         pencil47.fill()
         
         let pencil48 = UIBezierPath(rect: CGRect(x: 560, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil48.fill()
         
         let pencil49 = UIBezierPath(rect: CGRect(x: 0, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil49.fill()
         
         let pencil50 = UIBezierPath(rect: CGRect(x: 80, y: 480, width: 80, height: 80))
@@ -220,7 +224,7 @@ class BoardView: UIView {
         pencil50.fill()
         
         let pencil51 = UIBezierPath(rect: CGRect(x: 160, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil51.fill()
         
         let pencil52 = UIBezierPath(rect: CGRect(x: 240, y: 480, width: 80, height: 80))
@@ -228,7 +232,7 @@ class BoardView: UIView {
         pencil52.fill()
         
         let pencil53 = UIBezierPath(rect: CGRect(x: 320, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil53.fill()
         
         let pencil54 = UIBezierPath(rect: CGRect(x: 400, y: 480, width: 80, height: 80))
@@ -236,7 +240,7 @@ class BoardView: UIView {
         pencil54.fill()
         
         let pencil55 = UIBezierPath(rect: CGRect(x: 480, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil55.fill()
         
         let pencil56 = UIBezierPath(rect: CGRect(x: 560, y: 480, width: 80, height: 80))
@@ -248,7 +252,7 @@ class BoardView: UIView {
         pencil57.fill()
         
         let pencil58 = UIBezierPath(rect: CGRect(x: 80, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil58.fill()
         
         let pencil59 = UIBezierPath(rect: CGRect(x: 160, y: 560, width: 80, height: 80))
@@ -256,7 +260,7 @@ class BoardView: UIView {
         pencil59.fill()
         
         let pencil60 = UIBezierPath(rect: CGRect(x: 240, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil60.fill()
         
         let pencil61 = UIBezierPath(rect: CGRect(x: 320, y: 560, width: 80, height: 80))
@@ -264,7 +268,7 @@ class BoardView: UIView {
         pencil61.fill()
         
         let pencil62 = UIBezierPath(rect: CGRect(x: 400, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil62.fill()
         
         let pencil63 = UIBezierPath(rect: CGRect(x: 480, y: 560, width: 80, height: 80))
@@ -272,68 +276,31 @@ class BoardView: UIView {
         pencil63.fill()
         
         let pencil64 = UIBezierPath(rect: CGRect(x: 560, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
         pencil64.fill()
         
-        let blackRookImage = UIImage(named: "rook_chess_b")
-        blackRookImage?.draw(in: CGRect(x: 0, y: 0, width: 80, height: 80))
-        blackRookImage?.draw(in: CGRect(x: 560, y: 0, width: 80, height: 80))
+        drawPiece(col: 3, row: 0, imageName: "king_chess_b")
+        drawPiece(col: 4, row: 0, imageName: "queen_chess_b")
         
-        let blackKnightImage = UIImage(named: "knight_chess_b")
-        blackKnightImage?.draw(in: CGRect(x: 80, y: 0, width: 80, height: 80))
-        blackKnightImage?.draw(in: CGRect(x: 480, y: 0, width: 80, height: 80))
-        
-        let blackBishopImage = UIImage(named: "bishop_chess_b")
-        blackBishopImage?.draw(in: CGRect(x: 400, y: 0, width: 80, height: 80))
-        blackBishopImage?.draw(in: CGRect(x: 160, y: 0, width: 80, height: 80))
-        
-        let blackKingImage = UIImage(named: "king_chess_b")
-        blackKingImage?.draw(in: CGRect(x: 240, y: 0, width: 80, height: 80))
+        for i in 0..<2 {
+            drawPiece(col: i * 7 + 0, row: 0, imageName: "rook_chess_b")
+            drawPiece(col: i * 5 + 1, row: 0, imageName: "knight_chess_b")
+            drawPiece(col: i * 3 + 2, row: 0, imageName: "bishop_chess_b")
+        }
 
-        let blackQueenImage = UIImage(named: "queen_chess_b")
-        blackQueenImage?.draw(in: CGRect(x: 320, y: 0, width: 80, height: 80))
+        for i in 0..<8 {
+            drawPiece(col: i, row: 6, imageName: "pawn_chess_w")
+            drawPiece(col: i, row: 1, imageName: "pawn_chess_b")
+        }
         
-        let blackPawnImage = UIImage(named: "pawn_chess_b")
-        blackPawnImage?.draw(in: CGRect(x: 0, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 80, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 160, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 240, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 320, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 400, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 480, y: 80, width: 80, height: 80))
-        blackPawnImage?.draw(in: CGRect(x: 560, y: 80, width: 80, height: 80))
+        drawPiece(col: 0, row: 7, imageName: "rook_chess_w")
+        drawPiece(col: 7, row: 7, imageName: "rook_chess_w")
         
-        let whiteRookImage = UIImage(named: "rook_chess_w")
-        whiteRookImage?.draw(in: CGRect(x: 0, y: 560, width: 80, height: 80))
-        whiteRookImage?.draw(in: CGRect(x: 560, y: 560, width: 80, height: 80))
-        
-        let whiteKnightImage = UIImage(named: "knight_chess_w")
-        whiteKnightImage?.draw(in: CGRect(x: 80, y: 560, width: 80, height: 80))
-        whiteKnightImage?.draw(in: CGRect(x: 480, y: 560, width: 80, height: 80))
-        
-        let whiteBishopImage = UIImage(named: "bishop_chess_w")
-        whiteBishopImage?.draw(in: CGRect(x: 160, y: 560, width: 80, height: 80))
-        whiteBishopImage?.draw(in: CGRect(x: 400, y: 560, width: 80, height: 80))
-        
-        let whiteKingImage = UIImage(named: "king_chess_w")
-        whiteKingImage?.draw(in: CGRect(x: 240, y: 560, width: 80, height: 80))
-        
-        let whiteQueenImage = UIImage(named: "queen_chess_w")
-        whiteQueenImage?.draw(in: CGRect(x: 320, y: 560, width: 80, height: 80))
-        
-        let whitePawnImage = UIImage(named: "pawn_chess_w")
-        whitePawnImage?.draw(in: CGRect(x: 0, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 80, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 160, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 240, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 320, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 400, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 480, y: 480, width: 80, height: 80))
-        whitePawnImage?.draw(in: CGRect(x: 560, y: 480, width: 80, height: 80))
-
-
-        
+        drawPiece(col: 1, row: 7, imageName: "knight_chess_w")
+        drawPiece(col: 6, row: 7, imageName: "knight_chess_w")
+        drawPiece(col: 2, row: 7, imageName: "bishop_chess_w")
+        drawPiece(col: 5, row: 7, imageName: "bishop_chess_w")
+        drawPiece(col: 3, row: 7, imageName: "king_chess_w")
+        drawPiece(col: 4, row: 7, imageName: "queen_chess_w")
     }
-    
-
 }
--- chessTests/MathTests.swift
+++ chessTests/MathTests.swift
@@ -9,7 +9,17 @@
 import XCTest
 
 class MathTests: XCTestCase {
+    
+    func testAdd() {
+        let a = add(num1: 1, num2: 100)
+        print(a)
+    }
 
+    func add(num1: Int, num2: Int) -> Int {
+        let sum = num1 + num2
+        return sum
+    }
+    
     /*
      
      Collatz Conjecture
@@ -30,20 +40,20 @@ class MathTests: XCTestCase {
      
      3, 10, 5, 16, 8, 4, 2, 1, 4, 2, 1, 4, 2, 1, 4, 2, 1, 4, 2, 1, 4, 2, 1
      
-     100, 50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34,
-     
+     100, 50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 4,2, 1
  
     */
     func testCollatz() { // 13 % 5 = 3, 13 % 2 = 1, 42 % 2 = 0, 99 % 2 = 1
-        var c = 1000000
+        var c = 15
         while c != 1 {
             print("\(c), ", terminator: "")
-            if c % 2 == 1 {
-                c = 3 * c + 1
-            } else {
+            if c % 2 == 1 {// checking for odd numbers
+               c = 3 * c + 1
+            } else { // must be even numbers
                 c = c / 2
             }
         }
-        
+        print("\(c)")
     }
 }
+
