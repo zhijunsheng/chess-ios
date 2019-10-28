b2774109e1298b4ae21a157a8988a0c42aef94d3
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-10-27 20:34:52 -0500

    made a Prime number tester

--- chessTests/ChessBoardTests.swift
+++ chessTests/ChessBoardTests.swift
@@ -16,5 +16,31 @@ class ChessBoardTests: XCTestCase {
         print(chessBoard)
     }
     
-
+    func testIntArray() {
+        let numbers:[Int] = [3, 8, 11, 32] // numbers[0], numbers[1]
+        print(numbers.count)
+        var result = 0
+        for i in 0..<numbers.count {
+            result += numbers[i]
+        }
+        print(result)
+    }
+    
+    func isPrime(number: Int) -> Bool { // 371, 2,3,4,...,371/2
+        for i in 2..<number / 2 {
+            print("anything")
+            if number % i == 0 {
+                return false
+            }
+            
+        }
+        return true
+    }
+    
+    func testIsPrime() {
+        XCTAssertTrue(isPrime(number: 7))
+        XCTAssertFalse(isPrime(number: 14))
+        XCTAssertFalse(isPrime(number: 15))
+        XCTAssertTrue(isPrime(number: 17))
+    }
 }
