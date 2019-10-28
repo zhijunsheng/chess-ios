6e4621223239a5754027d62d1151b715dabb7131
ethan <ethanyiyaozhang@gmail.com>
2019-10-28 20:55:28 +0800

    We redrew pieces.

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -5,21 +5,6 @@ class BoardView: UIView {
     var originX: CGFloat = 123_456_789_123_456_789.0
     var originY: CGFloat = 173_738_383_838_839_498.0
     var cellSide: CGFloat = 80.0
-    /*
- 
-     (123, 234) x = 123, y = 234
-     (col = ?, row = ?)
-     
-     x = 440
-     col = 4
-     
-     
-     x = 751.2
-     col = 7
-     
-     Int(751.2 / cellSide)
- 
-    */
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let first = touches.first!
@@ -41,43 +26,35 @@ class BoardView: UIView {
             for j in 0..<4 {
                 drawSquare(col: i * 2, row: j * 2, color: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
                 drawSquare(col: i * 2 + 1, row: j * 2 + 1, color: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
-                drawSquare(col: i * 2 + 1, row: j * 2, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
-                drawSquare(col: i * 2, row: j * 2 + 1, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+                drawSquare(col: i * 2 + 1, row: j * 2, color: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
+                drawSquare(col: i * 2, row: j * 2 + 1, color: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
             }
         }
         
-        let queen_chess_bImg = UIImage(named: "queen_chess_b")
-        let king_chess_bImg = UIImage(named: "king_chess_b")
-        let king_chess_wImg = UIImage(named: "king_chess_w")
-        let queen_chess_wImg = UIImage(named: "queen_chess_w")
-        let rook_chess_wImg = UIImage(named: "rook_chess_w")
-        let knight_chess_wImg = UIImage(named: "knight_chess_w")
-        let bishop_chess_wImg = UIImage(named: "bishop_chess_w")
-        let rook_chess_bImg = UIImage(named: "rook_chess_b")
-        let knight_chess_bImg = UIImage(named: "knight_chess_b")
-        let bishop_chess_bImg = UIImage(named: "bishop_chess_b")
-        let pawn_chess_wImg = UIImage(named: "pawn_chess_w")
-        let pawn_chess_bImg = UIImage(named: "pawn_chess_b")
-        
-        king_chess_wImg?.draw(in: CGRect(x: originX + 3 * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
-        queen_chess_wImg?.draw(in: CGRect(x: originX + 4 * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
-        king_chess_bImg?.draw(in: CGRect(x: originX + 3 * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
-        queen_chess_bImg?.draw(in: CGRect(x: originX + 4 * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
-        
+        drawPiece(col: 3, row: 7, imageName: "king_chess_b")
+        drawPiece(col: 3, row: 0, imageName: "king_chess_w")
+        drawPiece(col: 4, row: 7, imageName: "queen_chess_b")
+        drawPiece(col: 4, row: 0, imageName: "queen_chess_w")
+
         for i in 0..<2 {
-            rook_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(7 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
-            knight_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(1 + 5 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
-            bishop_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(2 + 3 * i) * cellSide, y: originY + 0 * cellSide, width: cellSide, height: cellSide))
+            drawPiece(col: 7 * i, row: 0, imageName: "rook_chess_w")
+            drawPiece(col: 1 + 5 * i, row: 0, imageName: "knight_chess_w")
+            drawPiece(col: 2 + 3 * i, row: 0, imageName: "bishop_chess_w")
             
-            rook_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(7 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
-            knight_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(1 + 5 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
-            bishop_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(2 + 3 * i) * cellSide, y: originY + 7 * cellSide, width: cellSide, height: cellSide))
+            drawPiece(col: 7 * i, row: 7, imageName: "rook_chess_b")
+            drawPiece(col: 1 + 5 * i, row: 7, imageName: "knight_chess_b")
+            drawPiece(col: 2 + 3 * i, row: 7, imageName: "bishop_chess_b")
         }
         
         for i in 0..<8 {
-            pawn_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(i) * cellSide, y: originY + 6 * cellSide, width: cellSide, height: cellSide))
-            pawn_chess_wImg?.draw(in: CGRect(x: originX + CGFloat(i) * cellSide, y: originY + 1 * cellSide, width: cellSide, height: cellSide))
+            drawPiece(col: i, row: 6, imageName: "pawn_chess_b")
+            drawPiece(col: i, row: 1, imageName: "pawn_chess_w")
         }
+    }
+    
+    func drawPiece(col: Int, row: Int, imageName: String) {
+        let king_chess_bImg = UIImage(named: imageName)
+        king_chess_bImg?.draw(in: CGRect(x: originX + CGFloat(col) * cellSide, y: originY + CGFloat(row) * cellSide, width: cellSide, height: cellSide))
     }
     
     func drawSquare(col: Int, row: Int, color: UIColor) {
@@ -87,28 +64,3 @@ class BoardView: UIView {
     }
     
 }
-
-/*
- 
- ABCDE * 4 = EDCBA
-
- A = 2
- E = 8
- 
- ABCDE
- *   4
--------
- EDCBA
- 
-  2BCD8
-  *   4
- -------
-  8DCB2
- 
-   21978
-   *   4
-  -------
-   87912
- 
- 
- */
