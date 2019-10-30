e42e3aca74c52a2a377a7b53b9f117cc95f39301
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2019-10-30 22:02:06 +0800

    We learn six lines in the forloop.

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -87,30 +87,24 @@ class BoardView: UIView {
         drawSquare(col: 6, row: 7, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
         drawSquare(col: 7, row: 7, color: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
 
-        drawPiece(col: 0, row: 0, piece: "rook_chess_b")
-        drawPiece(col: 1, row: 0, piece: "knight_chess_b")
-        drawPiece(col: 2, row: 0, piece: "bishop_chess_b")
         drawPiece(col: 3, row: 0, piece: "king_chess_b")
         drawPiece(col: 4, row: 0, piece: "queen_chess_b")
-        drawPiece(col: 5, row: 0, piece: "bishop_chess_b")
-        drawPiece(col: 6, row: 0, piece: "knight_chess_b")
-        drawPiece(col: 7, row: 0, piece: "rook_chess_b")
-        
+        drawPiece(col: 3, row: 7, piece: "king_chess_w")
+        drawPiece(col: 4, row: 7, piece: "queen_chess_w")
         
         for i in 0..<8 { // column
             drawPiece(col: i, row: 1, piece: "pawn_chess_b")
             drawPiece(col: i, row: 6, piece: "pawn_chess_w")
-            
         }
-        
-        drawPiece(col: 0, row: 7, piece: "rook_chess_w")
-        drawPiece(col: 1, row: 7, piece: "knight_chess_w")
-        drawPiece(col: 2, row: 7, piece: "bishop_chess_w")
-        drawPiece(col: 3, row: 7, piece: "king_chess_w")
-        drawPiece(col: 4, row: 7, piece: "queen_chess_w")
-        drawPiece(col: 5, row: 7, piece: "bishop_chess_w")
-        drawPiece(col: 6, row: 7, piece: "knight_chess_w")
-        drawPiece(col: 7, row: 7, piece: "rook_chess_w")
+     
+        for i in 0..<2 {
+            drawPiece(col: i * 7, row: 0, piece: "rook_chess_b")
+            drawPiece(col: i * 5 + 1, row: 0, piece: "knight_chess_b")
+            drawPiece(col: i * 3 + 2, row: 0, piece: "bishop_chess_b")
+            drawPiece(col: i * 7, row: 7, piece: "rook_chess_w")
+            drawPiece(col: i * 5 + 1, row: 7, piece: "knight_chess_w")
+            drawPiece(col: i * 3 + 2, row: 7, piece: "bishop_chess_w")
+        }
     }
     
     func drawPiece(col: Int, row:Int, piece: String) {
