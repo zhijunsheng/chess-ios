a5dd317404cac01f38ae1bdb5cfe71acf07a6a18
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-10-29 20:56:13 -0400

    I also made and combined the black square for loops together into one loop!

--- chess/CanvasView.swift
+++ chess/CanvasView.swift
@@ -12,138 +12,18 @@ import UIKit
 class CanvasView: UIView {
 
     override func draw(_ rect: CGRect) {
-        
-        // Black Squares
-        
-        let blackSquare = UIBezierPath(rect: CGRect(x: 0, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare.fill()
-        
-        let blackSquare1 = UIBezierPath(rect: CGRect(x: 80, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare1.fill()
-        
-        let blackSquare2 = UIBezierPath(rect: CGRect(x: 160, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare2.fill()
-        
-        let blackSquare3 = UIBezierPath(rect: CGRect(x: 240, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare3.fill()
-        
-        let blackSquare4 = UIBezierPath(rect: CGRect(x: 320, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare4.fill()
-        
-        let blackSquare5 = UIBezierPath(rect: CGRect(x: 400, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare5.fill()
-        
-        let blackSquare6 = UIBezierPath(rect: CGRect(x: 480, y: 560, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare6.fill()
-        
-        let blackSquare7 = UIBezierPath(rect: CGRect(x: 560, y: 480, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare7.fill()
-        
-        let blackSquare8 = UIBezierPath(rect: CGRect(x: 480, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare8.fill()
-        
-        let blackSquare9 = UIBezierPath(rect: CGRect(x: 320, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare9.fill()
-        
-        let blackSquare10 = UIBezierPath(rect: CGRect(x: 160, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare10.fill()
-        
-        let blackSquare11 = UIBezierPath(rect: CGRect(x: 0, y: 400, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare11.fill()
-        
-        let blackSquare12 = UIBezierPath(rect: CGRect(x: 80, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare12.fill()
-        
-        let blackSquare13 = UIBezierPath(rect: CGRect(x: 240, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare13.fill()
-        
-        let blackSquare14 = UIBezierPath(rect: CGRect(x: 400, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare14.fill()
-        
-        let blackSquare15 = UIBezierPath(rect: CGRect(x: 560, y: 320, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare15.fill()
-        
-        let blackSquare16 = UIBezierPath(rect: CGRect(x: 0, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare16.fill()
-        
-        let blackSquare17 = UIBezierPath(rect: CGRect(x: 160, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare17.fill()
-        
-        let blackSquare18 = UIBezierPath(rect: CGRect(x: 320, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare18.fill()
-        
-        let blackSquare19 = UIBezierPath(rect: CGRect(x: 480, y: 240, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare19.fill()
-        
-        let blackSquare20 = UIBezierPath(rect: CGRect(x: 80, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare20.fill()
-        
-        let blackSquare21 = UIBezierPath(rect: CGRect(x: 240, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare21.fill()
-        
-        let blackSquare22 = UIBezierPath(rect: CGRect(x: 400, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare22.fill()
-        
-        let blackSquare23 = UIBezierPath(rect: CGRect(x: 560, y: 160, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare23.fill()
-        
-        let blackSquare24 = UIBezierPath(rect: CGRect(x: 0, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare24.fill()
-        
-        let blackSquare25 = UIBezierPath(rect: CGRect(x: 160, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare25.fill()
-        
-        let blackSquare26 = UIBezierPath(rect: CGRect(x: 320, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare26.fill()
-        
-        let blackSquare27 = UIBezierPath(rect: CGRect(x: 480, y: 80, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare27.fill()
-        
-        let blackSquare28 = UIBezierPath(rect: CGRect(x: 80, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare28.fill()
-        
-        let blackSquare29 = UIBezierPath(rect: CGRect(x: 240, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare29.fill()
-        
-        let blackSquare30 = UIBezierPath(rect: CGRect(x: 400, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare30.fill()
-        
-        let blackSquare31 = UIBezierPath(rect: CGRect(x: 560, y: 0, width: 80, height: 80))
-        #colorLiteral(red: 0.4143660946, green: 0.4143660946, blue: 0.4143660946, alpha: 1).setFill()
-        blackSquare31.fill()
+     
+        for i in 0..<4 {
+            drawBlackSquare(col: i * 2 + 1, row: 0)
+            drawBlackSquare(col: i * 2, row: 1)
+            drawBlackSquare(col: i * 2 + 1, row: 2)
+            drawBlackSquare(col: i * 2, row: 3)
+            drawBlackSquare(col: i * 2 + 1, row: 4)
+            drawBlackSquare(col: i * 2, row: 5)
+            drawBlackSquare(col: i * 2 + 1, row: 6)
+            drawBlackSquare(col: i * 2, row: 7)
+        }
 
-        
         for i in 0..<4 {
             drawSquare(col: i * 2, row: 0)
             drawSquare(col: i * 2 + 1, row: 1)
@@ -188,6 +68,12 @@ class CanvasView: UIView {
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         whiteSquare.fill()
         whiteSquare.stroke()
+    }
+    
+    func drawBlackSquare(col: Int, row: Int) {
+        let blackSquare = UIBezierPath(rect: CGRect(x: 80 * col, y: 80 * row, width: 80, height: 80))
+        #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
+        blackSquare.fill()
     }
 
     
