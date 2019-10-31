6a37fdf65dbe3c4fe4cffbc28f8407e8929b6a52
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-10-31 12:00:03 -0500

    created struct ChessPiece

---
+++ chess/ChessPiece.swift
@@ -0,0 +1,19 @@
+//
+//  ChessPiece.swift
+//  chess
+//
+//  Created by Grace Huang on 10/31/19.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct chessPiece {
+    let imageName: String
+    let col: Int
+    let row: Int
+    let isBlack: Bool
+    let pieceType: String // "Pawn", "P", "1"
+}
+
+
