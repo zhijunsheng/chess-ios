3636ff6346c12eaee573e719680c10f8b6e3e055
youjian zhou <yasuhiro@youjians-MacBook-Pro.local>
2019-12-14 09:38:35 -0500

    created an empty xcode project for chess

---
+++ chessTests/chessTests.swift
@@ -0,0 +1,36 @@
+//
+//  chessTests.swift
+//  chessTests
+//
+//  Created by youjian zhou on 2019-12-14.
+//  Copyright © 2019 youjian zhou. All rights reserved.
+//
+
+import XCTest
+@testable import chess
+
+class chessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
