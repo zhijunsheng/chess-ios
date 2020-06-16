3391ac480294d10e6d694af73b40deb3685e52ca
xiaobao <mariuspuzhouli@gmail.com>
2020-06-16 19:56:47 +0800

    we added 5 functions

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -1,6 +1,27 @@
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController, ChessDelegate {
+class ViewController: UIViewController, ChessDelegate, MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
     var game = GameRules()
     
     
