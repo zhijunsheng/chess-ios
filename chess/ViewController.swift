d1d6f6a22d19b6540c0183b364e7c19e00920155
daobao <lucashongyuanzhang@gmail.com>
2020-07-04 09:27:22 +0800

    we are ready to do the message sending and reciving

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -37,8 +37,6 @@ class ViewController: UIViewController, ChessDelegate {
         boardView.chessDelegate = self
         
         boardDeploy()
-        
-        togglePromotionButtons(show: false)
     }
     
     @IBAction func advertise(_ sender: Any) {
@@ -62,21 +60,6 @@ class ViewController: UIViewController, ChessDelegate {
         chessBrain.movePiece(frX: frX, frY: frY, toX: toX, toY: toY)
         boardView.piecesBoxShadow = chessBrain.piecesBox
         boardView.setNeedsDisplay()
-        
-        if movingPiece.rank == .pawn {
-            if movingPiece.isWhite {
-                if movingPiece.y == 7 {
-                    togglePromotionButtons(show: true)
-                    boardView.isUserInteractionEnabled = false
-                }
-                
-            } else if movingPiece.isWhite == false {
-               if movingPiece.y == 0 {
-                    togglePromotionButtons(show: true)
-                    boardView.isUserInteractionEnabled = false
-                }
-            }
-        }
     }
 
     func getMovingPiece(x: Int, y: Int) -> ChessPiece? {
@@ -86,48 +69,6 @@ class ViewController: UIViewController, ChessDelegate {
     func boardDeploy() {
         let bottonGap: CGFloat = 20
         let buttonLong: CGFloat = 176
-        
-        
-        promoteToQueenButton.frame.origin.x = bottonGap + 0 * buttonLong
-        promoteToQueenButton.frame.origin.y = view.frame.height - bottonGap - promoteToQueenButton.bounds.height
-        
-        promoteToKnightButton.frame.origin.x = bottonGap + 1 * buttonLong
-        promoteToKnightButton.frame.origin.y = view.frame.height - bottonGap - promoteToKnightButton.bounds.height
-        
-        promoteToBishopButton.frame.origin.x = bottonGap + 3 * buttonLong
-        promoteToBishopButton.frame.origin.y = view.frame.height - bottonGap - promoteToBishopButton.bounds.height
-        
-        promoteToRookButton.frame.origin.x = bottonGap + 2 * buttonLong
-        promoteToRookButton.frame.origin.y = view.frame.height - bottonGap - promoteToRookButton.bounds.height
-    }
-    
-    @IBAction func queenPressed(_ sender: UIButton) {
-        print("q u e e n")
-        chessBrain.promote(rank: .queen)
-    }
-    
-    @IBAction func rookPressed(_ sender: UIButton) {
-        print("r o o k")
-        chessBrain.promote(rank: .rook)
-    }
-    
-    @IBAction func knightPressed(_ sender: UIButton) {
-        print("k n i g h t")
-        chessBrain.promote(rank: .knight)
-    }
-    
-    @IBAction func bishopPressed(_ sender: UIButton) {
-        print("b i s h o p")
-        chessBrain.promote(rank: .bishop)
-    }
-    
-    
-    
-    func togglePromotionButtons(show: Bool) {
-        promoteToKnightButton.isHidden = !show
-        promoteToQueenButton.isHidden = !show
-        promoteToRookButton.isHidden = !show
-        promoteToBishopButton.isHidden = !show
     }
 }
 
@@ -162,11 +103,11 @@ extension ViewController: MCSessionDelegate {
 
 extension ViewController: MCBrowserViewControllerDelegate {
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        
+        dismiss(animated: true)
     }
     
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        
+        dismiss(animated: true)
     }
 }
 
--- chessTests/AgreementTests.swift
+++ chessTests/AgreementTests.swift
@@ -4,7 +4,7 @@ class AgreementTests: XCTestCase {
 
     
     /**
-    
+    0 -> 0
     */
     func testAgreement() {
         let msg = "p h 4 -> f 4"
