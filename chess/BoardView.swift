0e1ee63f656ecf8bff117e6cf49e782c114bc100
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2020-07-24 14:46:15 -0400

    made constants for the grid instead of hardcoded numbers

--- chess/BoardView.swift
+++ chess/BoardView.swift
@@ -10,17 +10,38 @@ import UIKit
 
 class BoardView: UIView {
     
+    let cell: CGFloat = 50
+    let originX: CGFloat = 50
+    let originY: CGFloat = 50
+    
+    
+    
+    
+    
+    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         
         for i in 0 ..< 9 {
-            path.move(to: CGPoint(x: i * 50 + 50, y: 50))
-            path.addLine(to: CGPoint(x: i * 50 + 50, y: 450))
+            path.move(to: CGPoint(x: CGFloat(i) * cell + originX, y: originY))
+            path.addLine(to: CGPoint(x: CGFloat(i) * cell + originX, y: cell * 8 + originY))
             
-            path.move(to: CGPoint(x: 50, y: i * 50 + 50))
-            path.addLine(to: CGPoint(x: 450, y: i * 50 + 50))
+            path.move(to: CGPoint(x: originX, y: CGFloat(i) * cell + originY))
+            path.addLine(to: CGPoint(x: cell * 8 + originX, y: CGFloat(i) * cell + originY))
         }
         
         path.stroke()
+        
+        let colourPath = UIBezierPath()
+        
+        colourPath.move(to: CGPoint(x: 300, y: 650))
+        colourPath.addLine(to: CGPoint(x: 450, y: 750))
+        colourPath.addLine(to: CGPoint(x: 150, y: 750))
+        colourPath.close()
+        
+        colourPath.move(to: CGPoint(x: 50, y: 50))
+        colourPath.addLine(to: CGPoint(x: 100, y: 750))
+        
+        colourPath.fill()
     }
 }
