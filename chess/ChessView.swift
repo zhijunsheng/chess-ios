181f77bb6936136ffec8d7ae2f0e67f0c8027072
Kenneth Wu <kennethwu@Kenneths-Macbook-Pro.local>
2022-02-14 17:14:17 -0500

    today I made the white pawns in chess

--- chess/ChessView.swift
+++ chess/ChessView.swift
@@ -15,9 +15,18 @@ class ChessView: UIView {
     var cell: CGFloat = -100
     
     override func draw(_ rect: CGRect) {
-        
         cell = bounds.width / 8
+        drawBoard()
         
+        for i in 0 ..< 8 {
+            let pawn = UIImage(named: "pawn_chess_w")
+            pawn?.draw(at: CGPoint(x: gx + cell * CGFloat(i), y: gy + cell * 6))
+        }
+        
+    }
+    
+    func drawBoard() {
+        UIColor.blue.setFill()
         for k in 0 ..< 4 {
             for i in 0 ..< 4 {
                 let pencil = UIBezierPath(rect: CGRect(x: gx + cell + cell * CGFloat(i * 2), y: gy + CGFloat(k) * (cell * 2), width: cell, height: cell))
@@ -29,4 +38,7 @@ class ChessView: UIView {
             }
         }
     }
+    
+    
+    
 }
