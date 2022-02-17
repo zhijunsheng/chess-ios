daaa4b790b9a3559e0c4d39ec56cd8092aab29ce
Kenneth Wu <kennethwu@Kenneths-Macbook-Pro.local>
2022-02-17 16:50:53 -0500

    today I made all the pieces, improved the code, added touch function and removed a bug

---
+++ chess/ChessGame.swift
@@ -0,0 +1,9 @@
+//
+//  ChessGame.swift
+//  chess
+//
+//  Created by Kenneth Wu on 2022-02-16.
+//  Copyright © 2022 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
---
+++ chess/ChessMan.swift
@@ -0,0 +1,9 @@
+//
+//  ChessMan.swift
+//  chess
+//
+//  Created by Kenneth Wu on 2022-02-16.
+//  Copyright © 2022 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
